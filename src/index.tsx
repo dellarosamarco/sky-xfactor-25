7d70280418b88cca8144c6e4f1351fc0e9aa2a63
Marco <96957446+dellarosamarco@users.noreply.github.com>
2025-09-28 22:18:54 +0200

    update

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.scss';
 import './index.scss';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import AppRouter from './routes/AppRouter';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <AppRouter />
-  </BrowserRouter>
+  </HashRouter>
 );
