bec9bb294028fd9272d374d41dafa9cfb6552914
Marco <96957446+dellarosamarco@users.noreply.github.com>
2025-10-02 21:40:21 +0200

    update

--- src/pages/Suggestions/Suggestions.tsx
+++ src/pages/Suggestions/Suggestions.tsx
@@ -12,22 +12,47 @@ const Suggestions = () => {
         <div className="page suggestions-page">
             <div className="suggestions-cards">
                 <div className="suggestions-cards-row">
+                    
                     <div className="suggestion-card suggestion-card-1">
-                        <p>1</p>
+                        <div className="card-inner">
+                            <div className="card-front">
+                                <p className='card-preview'>1</p>
+                            </div>
+                            <div className="card-back">
+                                <p>Parti dal positivo: cosa hai visto che ha funzionato bene? <br></br>Fai esempi concreti, non limitarti a dire "Bravo, mi piace"</p>
+                            </div>
+                        </div>
                     </div>
 
                     <div className="suggestion-card suggestion-card-2">
-                        <p>2</p>
+                        <div className="card-inner">
+                            <div className="card-front">
+                                <p className='card-preview'>2</p>
+                            </div>
+                            <div className="card-back">
+                                <p>Affronta i punti di miglioramento: Cosa non ha funzionato o poteva andare meglio? <br></br>Fai esempi specifici</p>
+                            </div>
+                        </div>
                     </div>
+
                 </div>
 
                 <div className="suggestion-card suggestion-card-3">
-                    <p>3</p>
+                    <div className="card-inner">
+                        <div className="card-front">
+                            <p className='card-preview'>3</p>
+                        </div>
+                        <div className="card-back">
+                            <p>Costruisci insieme il futuro: cosa potrebbe fare diversamente la prossima volta?</p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div className="suggestions-page-action">
-                <button className='button' onClick={() => onContinue()}>Ora registra il tuo feedback!</button>
+                <button className='button' onClick={onContinue}>
+                    Ora registra il tuo feedback!
+                </button>
             </div>
         </div>
     );
