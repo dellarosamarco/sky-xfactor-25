bffe0f2921d9dac307d8275c541f07dfc77b04ab
Marco Della Rosa <marco.dellarosa@skytv.it>
2025-10-03 13:46:53 +0200

    update

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,24 @@
 import { useBackgroundMusic } from "./../../context/BackgroundMusicContext"; 
 import "./Navbar.scss";
+import HomeIcon from "../../assets/home.svg";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../../firebase/authService";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   const { toggleMute, isMuted } = useBackgroundMusic();
+
+  const handleHomeClick = () => {
+    logout();
+    navigate("/");
+  }
 
   return (
     <nav className="navbar">
+      <div className="music-btn" onClick={handleHomeClick}>
+        <img src={HomeIcon} alt="Home" className="home-icon" width={40}/>
+      </div>
+
       <button className="music-btn" onClick={toggleMute}>
         <span className={`icon ${isMuted ? "muted" : "unmuted"}`}>
           {isMuted ? "🔇" : "🔊"}
